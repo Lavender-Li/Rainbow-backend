f1e90eb7a09a01a13da5ff49d253f7380c478ecb
Levana <47473643+Lavender-Li@users.noreply.github.com>
2022-07-04 00:15:50 +0800

    Create DetailActivity.java

---
+++ src/java/com/example/testproject/DetailActivity.java
@@ -0,0 +1,30 @@
+package com.example.testproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_view);
+    }
+
+    public void startDetection(View view){
+        startActivity(new Intent(this, ColorDetectionActivity.class));
+    }
+}
